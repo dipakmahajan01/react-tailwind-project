c4861e6096ff726486e1e2fe5d94117e0536d149
dipak <dipakmahajan2808200>
2025-01-19 10:50:00 +0530

    redirect related changes added in singup page

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,12 +4,20 @@ export const Dashboard = () => {
   //    const  [formaData,setFromData] = useState()
   return (
     <>
-      <div className="flex justify-between">
+       {/* how work tailwind greiclass */}
+      <div className="grid grid-cols-[1fr_300px_2fr] text-[#00000]">
         <div>chaild 1</div>
         <div>chaild 2</div>
         <div>chaild 3</div>
         <div>chaild 4</div>
       </div>
+      {/* <div className="bg-white rounded-lg p-6 shadow-xl"> 
+        hello world
+        <div>chaild 1</div>
+        <div>chaild 2</div>
+        <div>chaild 3</div>
+        <div>chaild 4</div>
+      </div> */}
     </>
   );
 };
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -9,7 +10,8 @@ const Signup = () => {
     confirmPassword: '',
   });
 
-  const [ setError] = useState('');
+  const navigate = useNavigate();
+  const [setError] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -39,58 +41,75 @@ const Signup = () => {
         </h2>
       </div>
       <div className='mt-10 sm:mx-auto'>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type='text'
-            className='border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 p-2'
-            name='username'
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type='email'
-              className='border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 p-2'
-            name='email'
-            
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type='password'
-              className='border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 p-2'
-            name='password'
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Confirm Password:</label>
-          <input
-            type='password'
-            name='confirmPassword'
-              className='border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 p-2'
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type='submit'  className='button-class border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 p-2'>Signup</button>
-      </form>
-        
+        <form onSubmit={handleSubmit} className='space-y-2'>
+          <div>
+            <label
+              htmlFor='username'
+              className='block text-sm/6 font-medium text-gray-900'
+            >
+              Username:
+            </label>
+            <input
+              type='username'
+              className='button-class border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 p-2 border-black-300'
+              name='username'
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor='email'
+              className='block text-sm/6 font-medium text-gray-900'
+            >
+              Email:
+            </label>
+            <input
+              type='email'
+              className='button-class border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 p-2 border-black-300'
+              name='email'
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label className='block text-sm/6 font-medium text-gray-900'>
+              Password:
+            </label>
+            <input
+              type='password'
+              className='button-class border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 p-2 border-black-300'
+              name='password'
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label className='block text-sm/6 font-medium text-gray-900'>
+              Confirm Password:
+            </label>
+            <input
+              type='password'
+              className='button-class border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 p-1 border-black-300'
+              name='confirm password'
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button
+            type='submit'
+            className='button-class border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 p-2'
+          >
+            <span onClick={() => navigate('/login')}>Sign up</span>
+          </button>
+        </form>
       </div>
     </div>
   );
-};                                      
+};
 
 export default Signup;
